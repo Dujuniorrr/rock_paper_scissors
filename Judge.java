d9d3af729b6934fbba3e9de5a0ba4c17cf99ba47
Dujuniorrr <durvaljunior117@gmail.com>
2023-08-07 19:55:21 -0300

    var23

--- Judge.java
+++ Judge.java
@@ -9,73 +9,97 @@ public class Judge {
 
     public static void main(String[] args) {
 
-        int portNumber = 6789;
+        int portNumber = 6789; // porta do processo
 
-        ArrayList<String> nicknamesOfPlayers = new ArrayList<String>();
-        ArrayList<InetAddress> addressOfPlayers = new ArrayList<InetAddress>();
-        ArrayList<Integer> portsOfPlayers = new ArrayList<Integer>();
+        ArrayList<String> nicknamesOfPlayers = new ArrayList<String>(); // lista dos nicksnames 
+        ArrayList<InetAddress> addressOfPlayers = new ArrayList<InetAddress>(); // lista dos ips
+        ArrayList<Integer> portsOfPlayers = new ArrayList<Integer>(); // lista das portas
 
-        DatagramSocket aSocket = null;
-        DatagramPacket request = null;
+        DatagramSocket aSocket = null; // socket
+        DatagramPacket request = null; // envia e recebe mensagem
 
         try {
-            byte[] buffer = new byte[1000];
-            aSocket = new DatagramSocket(portNumber);
+
+            byte[] buffer = new byte[1000]; // necessário para salvar os dados da mensagem
+            aSocket = new DatagramSocket(portNumber); // instancia o socket, e ele faz o bind explicito
+
             System.out.println("\nInicializando Juiz do jogo Pedra - Papel - Tesoura\nAguardando cadastro de jogador...");
 
             // Cadastrando jogadores
             while(addressOfPlayers.size() < 2){
-                buffer = new byte[1000];
-                request = new DatagramPacket(buffer, buffer.length); 
-                aSocket.receive(request);
+                buffer = new byte[1000]; // limpa os bytes 
+                request = new DatagramPacket(buffer, buffer.length); // prepara para o recebimento da mensagem
+                aSocket.receive(request); // espera a mensaggem chegar
 
-                nicknamesOfPlayers.add(new String(request.getData()).trim());
-                addressOfPlayers.add(request.getAddress());
-                portsOfPlayers.add(request.getPort());
+                nicknamesOfPlayers.add(new String(request.getData()).trim()); // adiciona na lista dos nicksnames 
+                addressOfPlayers.add(request.getAddress()); // adiciona na lista dos ips 
+                portsOfPlayers.add(request.getPort()); // adiciona na lista das portas
 
-                System.out.println("Jogador " + addressOfPlayers.size() +" (" + nicknamesOfPlayers.get( addressOfPlayers.size() -1) + ") cadastrado.");
+                System.out.println("Jogador " + addressOfPlayers.size() +" (" + nicknamesOfPlayers.get( addressOfPlayers.size() -1) + ") cadastrado."); // printa para o usuário
             }
 
-            int player = 0;  // 0 -> 1
-            int opponent = 1; // 1 -> 0
+            int player = 0;  // 0 -> 1 - incrementar
+            int opponent = 1; // 1 -> 0 - decrementar
 
             // Enviando oponentes
             while(player <= 1){ 
-                byte[] msg = nicknamesOfPlayers.get(opponent).getBytes(); // n2
-                request = new DatagramPacket(msg, msg.length, addressOfPlayers.get(player), portsOfPlayers.get(player));
-                aSocket.send(request);
-                player++;
-                opponent--;
+
+                byte[] msg = nicknamesOfPlayers.get(opponent).getBytes(); // pega o nickname do oponente em bytes
+
+                request = new DatagramPacket(msg, msg.length, addressOfPlayers.get(player), portsOfPlayers.get(player)); // prepara para enviar a mensagem
+
+                aSocket.send(request); //envia
+
+                player++; // jogador incrementa
+                opponent--; // oponente decrementa
             }
 
             //Inicia a partida
             int points = 0;
-            while(points < 2){
-                for(int i = 0; i < 2; i++){
-                    buffer = new byte[1000];
+
+            while(points < 2){ // enquanto nenhum jogador tiver mais de 1 ponto, a partida continua
+
+                for(int i = 0; i < 2; i++){ // pegar as escolhas dos jogadores
+
+                    buffer = new byte[1000]; //limpa os bytes para receber a escolha
+
+                    //pega a escolha
                     request = new DatagramPacket(buffer, buffer.length);
                     aSocket.receive(request); 
+
+                    // transforma em string
                     String choice = new String(request.getData()).trim();
             
                     //Envia as escolhas para os jogadores
-                    for(int j = 0; j < addressOfPlayers.size(); j++){
+                    for(int j = 0; j < addressOfPlayers.size(); j++){ //repete duas vezes
+
+
+                        // pega o oponente do jogador que enviou a mensagem
                         if(!request.getAddress().equals(addressOfPlayers.get(j)) || request.getPort() != portsOfPlayers.get(j).intValue()){
+
+                            //envia a escolha para o oponente
                             byte[] msg = choice.getBytes();
                             DatagramPacket requestChoiceOpponent = new DatagramPacket(msg, msg.length, addressOfPlayers.get(j), portsOfPlayers.get(j));
                             aSocket.send(requestChoiceOpponent);
+
                         }
+
                     }
                 }
 
                 //verifica se há um vencedor
-                for(int i = 0; i < 2; i++){
-                    buffer = new byte[1000];
+                for(int i = 0; i < 2; i++){ //repete duas vezes
+
+                    buffer = new byte[1000]; //limpa os bytes
+
                     request = new DatagramPacket(buffer, buffer.length);
                     aSocket.receive(request); 
-                    points = Integer.parseInt(new String(request.getData()).trim());
-                    if(points > 1){
+                    points = Integer.parseInt(new String(request.getData()).trim()); // recebe a pontuação
+
+                    if(points > 1){ //valor do ponto for maior que 2, ele para o loop, pois há um vencedor
                         break;
                     }
+
                 }
             }
         } catch (SocketException e) {
--- Player.java
+++ Player.java
@@ -9,53 +9,67 @@ public class Player {
     
     public static void main(String[] args) {
 
-        String nickname;
-        Scanner scan = new Scanner(System.in);
+        String nickname; // salva o nickname
+        Scanner scan = new Scanner(System.in); //instancia o scanner para pegar as entradas do usuário
 
         DatagramSocket aSocket = null;
         DatagramPacket request = null;
 
         try {
-            byte[] buffer = new byte[1000];
-            aSocket = new DatagramSocket();
+            byte[] buffer = new byte[1000]; //limpo os bytes
+            aSocket = new DatagramSocket(); //crio o socket, e faz um bind implicito
 
             System.out.print("Bem-vindo ao jogo Pedra - Papel - Tesoura\n Digite seu nickname: ");
             nickname = scan.nextLine();
-            byte[] msg = nickname.getBytes();
+            byte[] msg = nickname.getBytes(); //pega o nickname
 
+            //pega o ip e a porta do juiz que foram adicionados no terminal
             InetAddress aHost = InetAddress.getByName(args[0]);
-            int serverPort = Integer.parseInt(args[1]);
+            int serverPort = Integer.parseInt(args[1]); 
 
+            //prepara para enviar o nickname
             request = new DatagramPacket(msg, msg.length, aHost, serverPort);
 
+            //envia o nickname
             System.out.println("Aguarde seu oponente...");
             aSocket.send(request);
+
+            //prepara para receber o nickname do oponente
             request = new DatagramPacket(buffer, buffer.length);
+            //recebe o nickname
             aSocket.receive(request);
+
+            //printa o nickname do oponente
             String nicknameOpponent = new String(request.getData()).trim();
             System.out.println("Seu oponente é: " + nicknameOpponent);
 
+            //crio as variaveis de round, pontos do jogador e do oponente
             int rounds = 0;
             int points = 0;
             int pointsOpponent = 0;
 
             //Inicia a partida
-            while(points < 2 && pointsOpponent < 2){
-                rounds++;
-                int choice = 0;
+            while(points < 2 && pointsOpponent < 2){ //enquanto nenhum jogador fazer 2 pontos, a partida continua
 
-                //Escolhe uma opção
+                rounds++; //a rodada incrementa
+                int choice = 0; //e a escolha volta para zero
+
+                //Escolhe uma opção, deve ser igual 1, 2 ou 3
                 while(choice != 1 && choice != 2 && choice != 3){
+
                     System.out.print(
                     "\nIniciando rodada número [" + rounds +"]\n= Faça sua Escolha =\nPedra (digite 1)\nPapel (digite 2)\nTesoura (digite 3)\nDigite sua escolha: "
                     );
+
                     choice = scan.nextInt();
+
                     if(choice != 1 && choice != 2 && choice != 3){
                         System.out.print("\nEscolha uma opção válida!\n");
                     }
+
                 }
                
-                //Envia a escolha
+                //Envia a escolha do usuario
                 msg = Integer.toString(choice).getBytes();
                 request = new DatagramPacket(msg, msg.length, aHost, serverPort);
                 aSocket.send(request);
@@ -64,7 +78,10 @@ public class Player {
                 buffer = new byte[1000];
                 request = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(request);
+
                 int choiceOpponent = Integer.parseInt(new String(request.getData()).trim());
+
+                //printa as escolhas
                 System.out.println("\nVocê escolheu " + getChoice(choice));
                 System.out.println("Seu oponente escolheu " + getChoice(choiceOpponent));
 
@@ -79,24 +96,29 @@ public class Player {
                 //verifica quem ganhou a rodada
                 if (choice == choiceOpponent) {
                     System.out.println("\nSimbolos Iguais. Empate na rodada!\nNão será atribuido pontos para nenhum dos jogadores.");
+                    //se nenhum ganhou, ninguem ganha ponto
                 } else if (choice == 1 && choiceOpponent == 3
                         || choice == 2 && choiceOpponent == 1
                         || choice == 3 && choiceOpponent == 2) {
                     System.out.println("Você venceu esta rodada!");
-                    points++;
+                    points++; //se o jogador ganhou, ele ganha mais um ponto 
                 } else {
                     System.out.println("Você perdeu esta rodada!");
-                    pointsOpponent++;
+                    pointsOpponent++; //se o oponente ganhou, ele que ganha mais um ponto
                 }
 
+                //mostra o placar
                 System.out.println("\n=== PLACAR ===\n" + nickname +": " + points + "\n" + nicknameOpponent +": " + pointsOpponent + "\n=== === === ===");
 
                 //envia quantidade de pontos para o Juiz
                 msg = Integer.toString(points).getBytes();
                 request = new DatagramPacket(msg, msg.length, aHost, serverPort);
                 aSocket.send(request);
+
+                //se alguem fez dois pontos, já sai do laço
             }
 
+            //ocorre depois que sai do laço
             System.out.println("\nTemos um vencedor!\nFim de jogo.");
 
         } catch (SocketException e) {
